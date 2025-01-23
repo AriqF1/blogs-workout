7245aedf410ad2a3b441647c9595029226ca230d
Farhan Ariq <122168324+AriqF1@users.noreply.github.com>
2025-01-23 21:22:35 +0700

    initial commit

--- js/layouts.js
+++ js/layouts.js
@@ -1,17 +1,19 @@
-  window.onload = function() {
-      fetch('views/layouts/navbar.html')
-          .then(response => response.text())
-          .then(data => {
-              document.getElementById('navbar').innerHTML = data;
-          });
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('views/layouts/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        });
+
     fetch('views/layouts/info.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('info').innerHTML = data;
-          });
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('info').innerHTML = data;
+        });
+
     fetch('views/carousel/carousel.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('carousel').innerHTML = data;
-          });
-}
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('carousel').innerHTML = data;
+        });
+});
