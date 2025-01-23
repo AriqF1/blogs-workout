028aec23701e6f629c8408e17f11c008663c302e
Farhan Ariq <122168324+AriqF1@users.noreply.github.com>
2025-01-23 20:42:29 +0700

    primary

---
+++ js/layouts.js
@@ -0,0 +1,17 @@
+  window.onload = function() {
+      fetch('views/layouts/navbar.html')
+          .then(response => response.text())
+          .then(data => {
+              document.getElementById('navbar').innerHTML = data;
+          });
+    fetch('views/layouts/info.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('info').innerHTML = data;
+          });
+    fetch('views/carousel/carousel.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('carousel').innerHTML = data;
+          });
+}
